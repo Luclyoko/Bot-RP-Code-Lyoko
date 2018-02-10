7e08101108ff6f1c3c6e39650449e31bf8285dae
Luclyoko <36105236+Luclyoko@users.noreply.github.com>
2018-02-10 18:30:30 +0100

    Update main.js

--- main.js
+++ main.js
@@ -137,9 +137,26 @@ bot.on('message', message => {
 
         if(message.content.startsWith(prefix + "sms ")){
             if(message.member.roles.find("name", "Yumi")){
-               message.channel.sendMessage("Yumi detectee");
+               message.channel.sendMessage(message.content.replace(prefix + "sms ", "@here **Nouveau message de Yumi !** ```") + "```");
             }
-            else return;
+            if(message.member.roles.find("name", "Ulrich")){
+               message.channel.sendMessage(message.content.replace(prefix + "sms ", "@here **Nouveau message d'Ulrich !** ```") + "```");
+            }
+            if(message.member.roles.find("name", "Odd")){
+               message.channel.sendMessage(message.content.replace(prefix + "sms ", "@here **Nouveau message d'Odd !** ```") + "```");
+            }
+            if(message.member.roles.find("name", "Jérémie")){
+               message.channel.sendMessage(message.content.replace(prefix + "sms ", "@here **Nouveau message de Jérémie !** ```") + "```");
+            }
+            if(message.member.roles.find("name", "Aelita")){
+               message.channel.sendMessage(message.content.replace(prefix + "sms ", "@here **Nouveau message d'Aelita !** ```") + "```");
+            }
+            if(message.member.roles.find("name", "William")){
+               message.channel.sendMessage(message.content.replace(prefix + "sms ", "@here **Nouveau message de William !** ```") + "```");
+            }
+            if(message.member.roles.find("name", "Pauline")){
+               message.channel.sendMessage(message.content.replace(prefix + "sms ", "@here **Nouveau message de Pauline !** ```") + "```");
+            }
         }
 });
 
