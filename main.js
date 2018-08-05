6f29dcee7d37e45bd8937857793af64288dfbe0b
Luclyoko <36105236+Luclyoko@users.noreply.github.com>
2018-08-05 21:05:46 +0200

    Update main.js

--- main.js
+++ main.js
@@ -10,12 +10,17 @@ bot.on('ready', () => {
 bot.on('message', message => {
     if(message.author.bot) return;
         if(message.content.startsWith(prefix + "annoncesrp ")){
+            if(message.member.roles.find("name", "Admin")){
             message.channel.sendMessage(message.content.replace(prefix + "annoncesrp ", "<@&415119522597568512> **") + "**");
+            }
+                message.delete();
         }
                                      
         if(message.content.startsWith(prefix + "changestate ")){
+            if(message.member.roles.find("name", "Admin")){
             bot.user.setPresence({ game: { name: message.content.replace(prefix + "changestate ", ""), type: 0 } });
-            message.delete();
+            }
+                message.delete();
         }
     
         if(message.content.startsWith(prefix + "a ")){
@@ -25,39 +30,46 @@ bot.on('message', message => {
         }}
 
         if(message.content.startsWith(prefix + "pj ")){
+            if(message.member.roles.find("name", "Persos secondaires")){
             message.channel.sendMessage(message.content.replace(prefix + "pj ", "<:perejeremy:411920801999224853> **:** "));
             message.delete();
-        }
+        }}
         
         if(message.content.startsWith(prefix + "mj ")){
+            if(message.member.roles.find("name", "Persos secondaires")){
             message.channel.sendMessage(message.content.replace(prefix + "mj ", "**Mère de Jérémie :** "));
             message.delete();
-        }
+        }}
 
         if(message.content.startsWith(prefix + "jim ")){
+            if(message.member.roles.find("name", "Persos secondaires")){
             message.channel.sendMessage(message.content.replace(prefix + "jim ", "<:jim:410149301210054656> **:** "));
             message.delete();
-        }
+        }}
 
         if(message.content.startsWith(prefix + "sis ")){
+            if(message.member.roles.find("name", "Persos secondaires")){
             message.channel.sendMessage(message.content.replace(prefix + "sis ", "<:sissi:410149300929167360> **:** "));
             message.delete();
-        }
+        }}
 
         if(message.content.startsWith(prefix + "del ")){
+            if(message.member.roles.find("name", "Persos secondaires")){
             message.channel.sendMessage(message.content.replace(prefix + "del ", "<:delmas:410149296881532953> **:** "));
             message.delete();
-        }
+        }}
     
         if(message.content.startsWith(prefix + "mas ")){
+            if(message.member.roles.find("name", "Persos secondaires")){
             message.channel.sendMessage(message.content.replace(prefix + "mas ", "**Mr Massart :** "));
             message.delete();
-        }
+        }}
     
         if(message.content.startsWith(prefix + "yol ")){
+            if(message.member.roles.find("name", "Persos secondaires")){
             message.channel.sendMessage(message.content.replace(prefix + "yol ", "<:yolande:410531706295156737> **:** "));
             message.delete();
-        }
+        }}
 
         if(message.content.startsWith(prefix + "j ")){
             if(message.member.roles.find("name", "Jérémie")){
@@ -72,19 +84,22 @@ bot.on('message', message => {
         }}
     
         if(message.content.startsWith(prefix + "py ")){
+            if(message.member.roles.find("name", "Persos secondaires")){
             message.channel.sendMessage(message.content.replace(prefix + "py ", "**Mr Ishiyama :** "));
             message.delete();
-        }
+        }}
     
         if(message.content.startsWith(prefix + "my ")){
+            if(message.member.roles.find("name", "Persos secondaires")){
             message.channel.sendMessage(message.content.replace(prefix + "my ", "**Mme Ishiyama :** "));
             message.delete();
-        }
+        }}
     
         if(message.content.startsWith(prefix + "hir ")){
+            if(message.member.roles.find("name", "Persos secondaires")){
             message.channel.sendMessage(message.content.replace(prefix + "hir ", "<:hiroki:411920802787885057> **:** "));
             message.delete();
-        }
+        }}
 
         if(message.content.startsWith(prefix + "u ")){
             if(message.member.roles.find("name", "Ulrich")){
@@ -99,9 +114,10 @@ bot.on('message', message => {
         }}
 
         if(message.content.startsWith(prefix + "s ")){
+            if(message.member.roles.find("name", "Persos secondaires")){
             message.channel.sendMessage(message.content.replace(prefix + "s ", "**Sam :** "));
             message.delete();
-        }
+        }}
 
         if(message.content.startsWith(prefix + "w ")){
             if(message.member.roles.find("name", "William")){
@@ -110,39 +126,46 @@ bot.on('message', message => {
         }}
     
         if(message.content.startsWith(prefix + "mil ")){
+            if(message.member.roles.find("name", "Persos secondaires")){
             message.channel.sendMessage(message.content.replace(prefix + "mil ", "<:milly:410541886051450881> **:** "));
             message.delete();
-        }
+        }}
             
         if(message.content.startsWith(prefix + "tam ")){
+            if(message.member.roles.find("name", "Persos secondaires")){
             message.channel.sendMessage(message.content.replace(prefix + "tam ", "**Tamiya :** "));
             message.delete();
-        }
+        }}
             
         if(message.content.startsWith(prefix + "her ")){
+            if(message.member.roles.find("name", "Persos secondaires")){
             message.channel.sendMessage(message.content.replace(prefix + "her ", "<:herve:410854434642329600> **:** "));
             message.delete();
-        }
+        }}
     
         if(message.content.startsWith(prefix + "nic ")){
+            if(message.member.roles.find("name", "Persos secondaires")){
             message.channel.sendMessage(message.content.replace(prefix + "nic ", "<:nicolas:410862213163646977> **:** "));
             message.delete();
-        }
+        }}
     
         if(message.content.startsWith(prefix + "hel ")){
+            if(message.member.roles.find("name", "Persos secondaires")){
             message.channel.sendMessage(message.content.replace(prefix + "hel ", "**Héloïse :** "));
             message.delete();
-        }
+        }}
     
         if(message.content.startsWith(prefix + "hert ")){
+            if(message.member.roles.find("name", "Persos secondaires")){
             message.channel.sendMessage(message.content.replace(prefix + "hert ", "<:hertz:411920803723214859> **:** "));
             message.delete();
-        }
+        }}
     
         if(message.content.startsWith(prefix + "mey ")){
+            if(message.member.roles.find("name", "Persos secondaires")){
             message.channel.sendMessage(message.content.replace(prefix + "mey ", "**Mme Meyer :** "));
             message.delete();
-        }
+        }}
     
         if(message.content.startsWith(prefix + "p ")){
             if(message.member.roles.find("name", "Pauline")){
