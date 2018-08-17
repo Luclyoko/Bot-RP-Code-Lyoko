140f8dcf6ff9376a8596ff6e68954038f89a63aa
Luclyoko <36105236+Luclyoko@users.noreply.github.com>
2018-08-17 14:56:26 +0200

    Update main.js

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ bot.on('message', message => {
     if(message.author.bot) return;
         if(message.content.startsWith(prefix + "annoncesrp ")){
             if(message.member.roles.find("name", "Narrateur")){
-                message.channel.sendMessage(message.content.replace(prefix + "annoncesrp ", "@here __*") + "*__");
+                message.channel.sendMessage(message.content.replace(prefix + "annoncesrp ", "@here ***") + "***");
             }
                 message.delete();
         }
